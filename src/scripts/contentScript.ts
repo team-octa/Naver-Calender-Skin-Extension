a212dde1920a89350d502c531231b45c90713552
smartnation@naver.com <smartnation@naver.com>
2020-03-01 20:05:21 +0900

    feat: ⚡️ Add Font

--- src/scripts/contentScript.ts
+++ src/scripts/contentScript.ts
@@ -1,1 +1,5 @@
-document.body.style.filter = 'invert(1)';
+var link = document.createElement("link");
+link.href = "https://fonts.googleapis.com/css?family=Noto+Sans+KR:400,700&display=swap&subset=korean";
+link.type = "text/css";
+link.rel = "stylesheet";
+document.getElementsByTagName("head")[0].appendChild(link);
